9468ebaed8f40964e7cbca1671167c9128ef913b
rubatotree <zhuyutian@mail.ustc.edu.cn>
2024-09-17 22:59:27 +0800

    Modify shell scripts

--- lab1/gui/test.sh
+++ lab1/gui/test.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-mkdir ./build
-gcc ./src/main.c -o ./build/lab1_console
 for i in {1..8}; do
     echo "Testcase #$i:"
     echo "OUTPUT"
-    ./build/main -q < "../data/$i.in"
+	./build/lab1_console -q < "../data/$i.in"
     echo "ANSWER"
     cat "../data/$i.ans"
     echo
