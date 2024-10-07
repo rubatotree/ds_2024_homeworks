1308ec753c213b8e9d0c6ef0f0dd5c0fc2cea65c
rubatotree <zhuyutian@mail.ustc.edu.cn>
2024-10-07 09:31:51 +0000

    Implement the main framework

--- lab2/src/main.c
+++ lab2/src/main.c
@@ -3,13 +3,40 @@
 #include <stdlib.h>
 #include <math.h>
 
-///	Cycle Queue
-struct CustNode;
-typedef struct CustNode DataType;
+#define MAX 200010
+
+// Math functions
+int random_int()
+{
+	return rand() * RAND_MAX + rand();
+}
+
+int random_range(int l, int r)
+{
+	return l + random_int() % (r - l);
+}
+
+// DataTypes
+typedef int TimeType;
+typedef int AmountType;
 typedef unsigned int IndexType;
+typedef struct CustNode
+{
+	TimeType arrtime, durtime, leavetime;
+	AmountType amount; 
+	IndexType next;
+} CustNode, DataType;
 
+// Data to input
 size_t CHUNKSIZE = 4;
+AmountType total;
+TimeType closetime;
+TimeType  durtime_min,  durtime_max,
+		 interval_min, interval_max;
+AmountType amount_min,   amount_max;
+size_t event_number;
 
+///	Cycle Queue
 typedef struct CyQueue
 {
 	size_t maxsize;
@@ -117,6 +144,7 @@ DataType CyQueue_Front(CyQueue* q)
 	return q->data[q->head];
 }
 
+/*
 void CyQueue_PrintData(DataType data)
 {
 	printf("%d", data);
@@ -133,18 +161,59 @@ void CyQueue_Print(CyQueue* q)
 	}
 	printf("\n");
 }
+*/
 
-// Cust
-typedef unsigned int TimeType;
-typedef long long AmountType;
-typedef struct CustNode
+// Functions
+CustNode eventlist[MAX];
+
+void eventlist_GenerateDay1()
 {
-	TimeType arrtime, durtime, leavetime;
-	AmountType amount; 
-} CustNode;
+	TimeType arrtime = 0;
+	for(IndexType i = 0; i < event_number; i++)
+	{
+		eventlist[i].next = i + 1;
+		eventlist[i].arrtime = arrtime;
+		eventlist[i].durtime = random_range(durtime_min, durtime_max + 1);
+		eventlist[i].amount = random_range(amount_min, amount_max + 1);
+		eventlist[i].leavetime = -1;
 
+		TimeType interval = random_range(interval_min, interval_max + 1);
+		arrtime += interval;
+	}
+}
+
+// Main Program
+void eventlist_output()
+{
+	printf("Event \t| Arrtime \t| Durtime \t| Amount \t| Leavetime\n");
+	for(IndexType i = 0; i < event_number; i++)
+	{
+		printf("%u \t| %u\t| %u\t| %d\t| %u\t\n", i, eventlist[i].arrtime, eventlist[i].durtime, eventlist[i].amount, eventlist[i].leavetime);
+	}
+}
+
+void process()
+{
+	eventlist_GenerateDay1();
+	eventlist_output();
+}
 
 int main()
 {
+	printf("Please input the total amount:\n");
+	scanf("%d\n", &total);
+	printf("Please input the close time:\n");
+	scanf("%d\n", &closetime);
+	printf("Please input the min&max of durtime:\n");
+	scanf("%d%d\n", &durtime_min, &durtime_max);
+	printf("Please input the min&max of amount:\n");
+	scanf("%d%d\n", &amount_min, &amount_max);
+	printf("Please input the min&max of interval:\n");
+	scanf("%d%d\n", &interval_min, &interval_max);
+	printf("Please input the CHUNKSIZE:\n");
+	scanf("%d\n", &CHUNKSIZE);
+	printf("Please input the number of events in day 1\n");
+	scanf("%u", &event_number);
+	process();
 	return 0;
 }
